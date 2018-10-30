08a2816e44258489b29f8176a98148d32d471d77
TommyLike <tommylikehu@gmail.com>
2018-10-30 16:03:52 +0800

    Update AWS region for tests

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -42,9 +42,9 @@ variable "key_name" {
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "aws_region" {
-  default = "eu-west-1"
+  default = "ap-northeast-1"
   description = "AWS region to launch servers."
 }
 variable "aws_availability_zone" {
-  default = "eu-west-1b"
+  default = "ap-northeast-1a"
 }
